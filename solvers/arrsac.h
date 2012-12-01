d6f3cc73f8388014044f9431192b105a24528049
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2012-11-30 16:20:42 -0800

    .

--- solvers/arrsac.h
+++ solvers/arrsac.h
@@ -1,19 +1,20 @@
 #ifndef SOLVERS_ARRSAC_H
 #define SOLVERS_ARRSAC_H
 
-int GenerateInitialHypothesisSet() {
+void GenerateInitialHypothesisSet(vector<Model>* accepted_hypotheses) {
   int k = 1;
   int m_prime = m;
   int count_in = 0;
   int flag_in = 0;
   int max_inner_ransac_its = 10000;
   while (k > m_prime) {
+    Model hypothesis;
     if (flag_in == 0) {
       // generate hypothesis h(k) with k-th PROSAC sample
-      GenerateHypothesisProsac();
+      hypothesis = GenerateHypothesisProsac();
     } else {
       // generate hypothesis h(k) with sampling subset from input
-      GenerateHypothesisSampling();
+      hypothesis = GenerateHypothesisSampling();
       count_in++;
       if (count_in == max_inner_ransac_its) {
         // reset count_in, flag_in
@@ -21,8 +22,13 @@ int GenerateInitialHypothesisSet() {
     }
 
     // evaluate hypothesis h(k) with SPRT
+    bool sprt_test = SPRT(hypothesis);
     // if hypothesis h(k) is rejected:
-    //   re-estimate params of SPRT (if required)
+    if (!sprt_test) {
+      // ???
+      //   re-estimate params of SPRT (if required)
+    }
+
     // else if hypothesis h(k) is accepted and has the largest support so far
     //   flag_in = 1
     //   count_in = 0
@@ -37,28 +43,10 @@ int GenerateInitialHypothesisSet() {
 void compute() {
   // Initialize:
   //   set values for M and B
-  //   set parameters for SPRT test, calculate initial value of A
-  // Generate Initial Hypothesis Test
-  // Preemptive Evaluation
-  //   for i = B+1...N do:
-  //     Set p = num hypoth remaining, n = min( f(i), p/2)
-  //     Rorder and select hypothesis h(1)...h(n)
-  //     if n == 1
-  //       break with remaining hypothesis as top
-  //   score the hypotheses using data point i
-  //   if (i mod B) == 0
-  //     Calculate best inlier ratio e' and num hypotheses M' (eq. 1)
-  //     M' = max(M,M')
-  //     if M' > k
-  //       Generate and evaluate M' - k new hypotheses on i data points
-  //       k = M'
-
-
-  // Initialize:
-  //   set values for M and B
   int max_candidate_hyps = 500;
   int block_size = 100;
   //   set parameters for SPRT test, calculate initial value of A
+  SetSPRTParameters();
 
   // Generate Initial Hypothesis Test
   // k = total number of hypotheses generated in initial stage
@@ -72,7 +60,10 @@ void compute() {
     //     n = min( f(i), p/2)
     int f_i = floor(max_candidate_hyps * pow(2, -1.0*floor(i/block_size)));
     int n = min(f_i, p/2);
+
     //     Rorder and select hypothesis h(1)...h(n)
+    hypotheses.clear();
+    hypotheses = GetTopKHypotheses(n);
     //     if n == 1
     //       break with remaining hypothesis as top
     if (n == 1) {
