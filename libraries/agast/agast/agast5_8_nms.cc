06a49270495b7a3891e565fe7e33bf1cc0e1ea3a
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2013-06-05 11:47:45 -0700

    Changed agast library to match the one from BRISK

--- libraries/agast/agast/agast5_8_nms.cc
+++ libraries/agast/agast/agast5_8_nms.cc
@@ -1,31 +1,33 @@
-//
-//    agast5 - AGAST, an adaptive and generic corner detector based on the
-//              accelerated segment test for a 8 pixel mask
-//
-//    Copyright (C) 2010  Elmar Mair
-//    All rights reserved.
-//
-//    Redistribution and use in source and binary forms, with or without
-//    modification, are permitted provided that the following conditions are met:
-//        * Redistributions of source code must retain the above copyright
-//          notice, this list of conditions and the following disclaimer.
-//        * Redistributions in binary form must reproduce the above copyright
-//          notice, this list of conditions and the following disclaimer in the
-//          documentation and/or other materials provided with the distribution.
-//        * Neither the name of the <organization> nor the
-//          names of its contributors may be used to endorse or promote products
-//          derived from this software without specific prior written permission.
-//
-//    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-//    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-//    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-//    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-//    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-//    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-//    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-//    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+    agast5 - AGAST, an adaptive and generic corner detector based on the
+             accelerated segment test for a 8 pixel mask
+
+    Copyright (c) 2010, Elmar Mair
+    All rights reserved.
+   
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+       * Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+       * Neither the name of the owner nor the names of its contributors may be 
+         used to endorse or promote products derived from this software without 
+         specific prior written permission.
+   
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -33,8 +35,9 @@
 #include "agast5_8.h"
 
 using namespace std;
+using namespace agast;
 
-//using also bisection as proposed by Edward Rosten in FAST,
+//using also bisection as propsed by Edward Rosten in FAST,
 //but it is based on the OAST
 int AgastDetector5_8::cornerScore(const unsigned char* p)
 {
--- libraries/agast/agast/oast9_16.cc
+++ libraries/agast/agast/oast9_16.cc
@@ -1,31 +1,32 @@
-//
-//    oast9 - OAST, an optimal corner detector based on the
-//              accelerated segment test for a 16 pixel mask
-//
-//    Copyright (C) 2010  Elmar Mair
-//    All rights reserved.
-//
-//    Redistribution and use in source and binary forms, with or without
-//    modification, are permitted provided that the following conditions are met:
-//        * Redistributions of source code must retain the above copyright
-//          notice, this list of conditions and the following disclaimer.
-//        * Redistributions in binary form must reproduce the above copyright
-//          notice, this list of conditions and the following disclaimer in the
-//          documentation and/or other materials provided with the distribution.
-//        * Neither the name of the <organization> nor the
-//          names of its contributors may be used to endorse or promote products
-//          derived from this software without specific prior written permission.
-//
-//    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-//    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-//    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-//    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-//    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-//    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-//    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-//    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+    oast9 - OAST, an optimal corner detector based on the
+            accelerated segment test for a 16 pixel mask
+
+    Copyright (c) 2010, Elmar Mair
+    All rights reserved.
+   
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+       * Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+       * Neither the name of the owner nor the names of its contributors may be 
+         used to endorse or promote products derived from this software without 
+         specific prior written permission.
+   
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 //machine generated code
 //probability of an equal pixel on the Bresenham's circle: 0.33
@@ -40,8 +41,9 @@
 #include "oast9_16.h"
 
 using namespace std;
+using namespace agast;
 
-vector<CvPoint>& OastDetector9_16::detect(const unsigned char* im)
+void OastDetector9_16::detect(const unsigned char* im, vector<CvPoint>& corners_all)
 {
 	int total=0;
 	int nExpectedCorners=corners_all.capacity();
@@ -2134,7 +2136,6 @@ vector<CvPoint>& OastDetector9_16::detect(const unsigned char* im)
 			total++;
 		}									
 	}										
-	return corners_all;
 }
 
 //end of file
