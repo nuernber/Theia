a35352c6eb9b85828a399bbc70e7ce8dbb1a75bc
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2014-04-17 15:23:47 -0700

    updated some compiler settings in the build

--- cmakeFindModules/OptimizeTheiaCompilerFlags.cmake
+++ cmakeFindModules/OptimizeTheiaCompilerFlags.cmake
@@ -32,10 +32,42 @@
 # Please contact the author of this library if you have any questions.
 # Author: Chris Sweeney (cmsweeney@cs.ucsb.edu)
 #
+# Much of this file was modified from Ceres Solver which has the license below:
+#
+# Ceres Solver - A fast non-linear least squares minimizer
+# Copyright 2010, 2011, 2012 Google Inc. All rights reserved.
+# http://code.google.com/p/ceres-solver/
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Google Inc. nor the names of its contributors may be
+#   used to endorse or promote products derived from this software without
+#   specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: keir@google.com (Keir Mierle)
+
 # These two methods are macros so that they can modify global parameters
 # (functions cannot do that easily with CMake). Much of the compilation
 # parameter setup was borrowed and possibly modified from Ceres Solver.
-macro(OptimizeCompilerFlags)
+macro(OptimizeTheiaCompilerFlags)
   # Change the default build type from Debug to Release, while still
   # supporting overriding the build type.
   #
@@ -174,4 +206,4 @@ macro(OptimizeCompilerFlags)
       "${CMAKE_CXX_FLAGS} -Werror -Wall -Wextra -Wno-unknown-pragmas -Wno-sign-compare -Wno-unused-parameter -Wno-missing-field-initializers")
   ENDIF (UNIX)
 
-endmacro(OptimizeCompilerFlags)
+endmacro(OptimizeTheiaCompilerFlags)
